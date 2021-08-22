98871bde88bcdd58ce0487e8459eb0c3505f0290
kcordovas <kcje456@hotmail.com>
2021-08-22 17:29:58 -0500

    Unit 2: Layouts -> User Input Part 1: 3Class and Inheritance with 4Create design XML

---
+++ app/src/main/java/com/kevcordovas/studycertificationandroiddeveloperapp/kotlinbasics/layouts/userinput1/TipCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.kevcordovas.studycertificationandroiddeveloperapp.kotlinbasics.layouts.userinput1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kevcordovas.studycertificationandroiddeveloperapp.R
+
+class TipCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tip_calculator)
+    }
+}
