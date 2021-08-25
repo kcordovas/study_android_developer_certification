6877a288058f794b5cb789f914f0fd331811a735
kcordovas <kcordovas77@gmail.com>
2021-08-24 20:51:05 -0500

    Unit 2: Layouts -> User Input Part 2: 3Create a more polished UX, finis tip calculator app

--- app/src/main/java/com/kevcordovas/studycertificationandroiddeveloperapp/kotlinbasics/layouts/userinput1/TipCalculatorActivity.kt
+++ app/src/main/java/com/kevcordovas/studycertificationandroiddeveloperapp/kotlinbasics/layouts/userinput1/TipCalculatorActivity.kt
@@ -1,7 +1,11 @@
 package com.kevcordovas.studycertificationandroiddeveloperapp.kotlinbasics.layouts.userinput1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.kevcordovas.studycertificationandroiddeveloperapp.R
 import com.kevcordovas.studycertificationandroiddeveloperapp.databinding.ActivityTipCalculatorBinding
 import java.text.NumberFormat
@@ -16,10 +20,11 @@ class TipCalculatorActivity : AppCompatActivity() {
         binding = ActivityTipCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.buttonCalculate.setOnClickListener{ onTipCalculate()}
+        binding.costOfServiceEditText.setOnKeyListener { view, keyScope, _ -> handleKey(view, keyScope) }
     }
 
     private fun onTipCalculate() {
-        val stringIntTextField = binding.inputCostOfService.text.toString()
+        val stringIntTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringIntTextField.toDoubleOrNull()
         if (cost == null) {
             displayTip(0.0)
@@ -43,5 +48,15 @@ class TipCalculatorActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.textTipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKey(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
